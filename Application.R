5540db18869e10a6a84b134b6fcc0f87edde080d
Niccolò Cao <niccolo.cao.research@gmail.com>
2026-04-04 23:02:56 +0200

    Update Application.R

--- Application.R
+++ Application.R
@@ -3,6 +3,10 @@ library(dplyr)
 library(tidyr)
 library(knitr)
 library(kableExtra)
+library(ggplot2)
+library(RColorBrewer)
+library(patchwork)
+library(tikzDevice)
 source("pairaveFuns.R")
 
 
@@ -156,14 +160,14 @@ tab_stime <- make_table_est_se_items_rows(est_full_nocov, se_full_nocov, digits 
 
 # Wald tests
 tab <- wald_table_raw(est_interest_adj, se_interest, add_fdr_corr = TRUE)
-
+tab_use = tab
 
 # Wald on fisher z for correlations Cor(b_ik1, s_i) and Cor(b_ik2, s_i)
 corr_frail <- est_interest_adj[grepl("frailty", names(est_interest_adj))]
 se_frail   <- se_interest[names(corr_frail)]
 
 tab_fisher <- wald_fisher_from_r(corr_frail, se_frail)
-tab_fisher
+
 
 
 # Correlations Cor(b_ik2 - b_ik1, s_i)
@@ -223,4 +227,207 @@ res_shiftcov_EQ$p
 res_shiftcov_KOOS$W
 res_shiftcov_KOOS$df
 res_shiftcov_KOOS$p
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################## Correlation plots ######################################################################
+
+itemnamesEQ   <- c('Mobility','SelfCare','UsualAct','PainDisc','AnxDepr',"Healt")
+itemnamesKOOS <- c('OutBed','PutSocks','UpSit','Bend','Twist','Kneel','Squat')
+
+origEQ   <- c('Mobility','SelfCare','UsualActivities','PainorDiscomfort','Anxietyordepression',"Perceivedcurrenthealthstatus")
+origKOOS <- c('Gettingoutofbed','Puttingonsocks','Gettingupfromsitting',
+              'Bendingtowardthefloor_pickinguponobjectfromtheground',
+              'Twisting_pivotingontheinjuredknee','Kneeling','Squatting')
+
+short_map_base <- setNames(c(itemnamesEQ, itemnamesKOOS), c(origEQ, origKOOS))
+
+add_abc <- TRUE 
+labs_I0 <- make_labs_time(est_interest, time = "I0", include_frailty = TRUE)
+sd_I0   <- make_sd_vec_for_labs(est_interest, labs_I0)
+RP_I0   <- make_RP_from_tab(tab, labs_I0, p_col = c("p_fisher","p"))
+
+
+I0frail <- plot_corr_circles_lower(
+  R = RP_I0$R,
+  P = RP_I0$P,
+  sd_vec = sd_I0,
+  title = panel_title("a","RE at baseline + Frailty"),
+  label_fun = make_pretty_label_vec,
+  block_split = 6,
+  circle_max = 6,
+  r_text_size = 3.5,
+  sd_text_size = 3.5,
+  block_text_size = 4.8
+) + theme_corr_text(
+  axis_x_size = 12,
+  axis_y_size = 12,
+  plot_title_size = 15,
+  legend_title_size = 10,
+  legend_text_size = 9
+)
+
+# --- post + frailty
+labs_Ipost <- make_labs_time(est_interest, time = "Ipost", include_frailty = TRUE)
+sd_Ipost   <- make_sd_vec_for_labs(est_interest, labs_Ipost)
+RP_Ipost   <- make_RP_from_tab(tab, labs_Ipost, p_col = c("p_fisher","p"))
+
+
+Ipostfrail <- plot_corr_circles_lower(
+  R = RP_Ipost$R,
+  P = RP_Ipost$P,
+  sd_vec = sd_Ipost,
+  title = panel_title("b","RE at post-intervention + Frailty"),
+  label_fun = make_pretty_label_vec,
+  block_split = 6,
+  circle_max = 6,
+  r_text_size = 3.5,
+  sd_text_size = 3.5,
+  block_text_size = 4.8
+) + theme_corr_text(
+  axis_x_size = 12,
+  axis_y_size = 12,
+  plot_title_size = 15,
+  legend_title_size = 10,
+  legend_text_size = 9
+)
+
+I0frail
+Ipostfrail
+
+
+
+rows_I0   <- make_labs_time(est_interest, time = "I0",   include_frailty = FALSE)
+cols_Ipost<- make_labs_time(est_interest, time = "Ipost",include_frailty = FALSE)
+
+cross <- make_cross_RP_from_tab(
+  tab = tab,
+  rows = rows_I0,
+  cols = cols_Ipost,
+  p_col = c("p_fisher","p"),
+  set_oob_to_NA = TRUE
+)
+
+p_cross <- plot_corr_circles_rect(
+  R = cross$R,
+  P = cross$P,
+  title = panel_title("c","Baseline × Post-intervention"),
+  x_title = "Post-intervention",
+  y_title = "Baseline",
+  y_axis_position = "right",
+  row_split = 6,
+  col_split = 6,
+  block_line_width = 1.8,
+  circle_max = 6,
+  oob_cells = cross$oob_cells,
+  r_text_size = 3.5,
+  show_y_labels = FALSE,
+  right_title_size = 5.6,
+  right_title_x = 0.98
+) + theme_corr_text(
+  axis_x_size = 12,
+  axis_y_size = 12,
+  axis_title_y_size = 12,
+  axis_title_x_size = 14,
+  plot_title_size = 15,
+  legend_title_size = 10,
+  legend_text_size = 9
+)
+
+
+
+p_all <- (I0frail + guides(size = "none", colour = "none") |
+            Ipostfrail + guides(size = "none", colour = "none") |
+            p_cross) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "right")
+
+
+ 
+
+no_y_left <- theme(
+  axis.text.y = element_blank(),
+  axis.ticks.y = element_blank()
+)
+
+no_y_right <- theme(
+  axis.text.y.right  = element_blank(),
+  axis.ticks.y.right = element_blank()
+)
+
+p_all <- (
+  I0frail + guides(size = "none", colour = "none") |
+    (Ipostfrail + guides(size = "none", colour = "none") + no_y_left) |
+    p_cross
+) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "right")
+
+tikz("/home/nc/MEGA/BioPsychometrics/biomlatex/corrplots.tex", width = 14, height = 6.5, standAlone = FALSE,engine = "xetex")
+print(p_all)
+dev.off()
+
+
+labs_resid <- c(origEQ, origKOOS)
+
+R_resid <- corr_resid_matrix_simple(
+  est_interest_adj,
+  base = "corr_resid",
+  sep = "__"
+)
+
+labs_resid <- labs_resid[labs_resid %in% rownames(R_resid)]
+R_resid <- R_resid[labs_resid, labs_resid, drop = FALSE]
+R_resid <- 0.5 * (R_resid + t(R_resid))
+
+P_resid <- make_P_resid_from_tab(
+  tab = tab_use,
+  labs = labs_resid,
+  base = "corr_resid",
+  sep = "__",
+  p_col = c("p_fdr", "p_fisher", "p")
+)
+
+p_resid <- plot_corr_circles_lower(
+  R = R_resid,
+  P = P_resid,
+  sd_vec = setNames(rep(1, length(labs_resid)), labs_resid),
+  title = "Residual correlations",
+  label_fun = make_pretty_label_vec,
+  block_split = 6,
+  circle_max = 6,
+  r_text_size = 3.5,
+  sd_text_size = 3.5,
+  
+)+ theme_corr_text(
+  axis_x_size = 12,
+  axis_y_size = 12,
+  plot_title_size = 15,
+  legend_title_size = 10,
+  legend_text_size = 9
+)
+
+p_resid
+
+
+tikz("/home/nc/MEGA/BioPsychometrics/biomlatex/residcor.tex", width = 6, height = 6, standAlone = FALSE,engine = "xetex")
+print(p_resid)
+dev.off()
 
